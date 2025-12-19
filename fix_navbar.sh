119cab33e6b8aeadcc38527a6595ea4efde5f01f
Sspanwar31 <Sspanwar31@users.noreply.github.com>
2025-12-19 12:46:46 +0000

    Auto-Backup via Admin Panel: 2025-12-19T12:46:46.640Z

--- fix_navbar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Fix infinite loop in Navbar.tsx by replacing problematic useEffect
-echo "Fixing Navbar infinite loop issues..."
-
-# Use ed for exact text replacement
-ed -i '22,55s/}, \[\])/useEffect(() => {/},[isAuthenticated])/useEffect(() => {/},[isAuthenticated])' src/components/layout/Navbar.tsx
-
-echo "Fixed first useEffect infinite loop"
